39a6a597191c054791a1f97e6d3a72e5fb798b4f
Kovsnkiw <delavoski@proton.me>
2025-12-28 14:52:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const texto =
-    "Eu não sei muito bem onde isso vai pode levar kk, mas sei que gosto de quando eu converso com você. Gosto de saber como foi seu dia, o que você anda fazendo, o que você gosta e o que não gosta. Gosto de saber dos seus sonhos e planos, e de compartilhar os meus também. Gosto de rir das suas piadas, mesmo quando são bobas, e de ouvir suas histórias, mesmo quando são longas. Gosto de sentir sua presença, mesmo que seja só por mensagem. Gosto de você, do jeito que você é, com todas as suas qualidades e defeitos. Gosto de você por ser você, e isso já é o suficiente para mim.";
+    "Eu não sei muito bem onde isso vai dar sabes? Nossa conversa diárias, nossas saídas repentinas, eu sinto uma conexão e sei que gosto de quando eu converso com você. Gosto de saber como foi seu dia, se você acordou bem, se você vai para algum lugar, o que você anda fazendo, o que você gosta e o que não gosta. Gosto de saber dos seus sonhos e planos (até os planos que você ainda não me contou haha), e de compartilhar os meus também. Gosto de rir contigo, mesmo quando são bobas, e de ouvir suas histórias, mesmo quando são longas. Gosto de sentir sua presença, mesmo que seja só por mensagem. Gosto de você, do jeito que você é, com todas as suas qualidades e defeitos. Gosto de você por ser você, e isso já é o suficiente para mim. Obrigado, Damares. ❤️";
 
 let i = 0;
 
@@ -16,3 +16,4 @@ function escrever() {
         setTimeout(escrever, 50);
     }
 }
+
